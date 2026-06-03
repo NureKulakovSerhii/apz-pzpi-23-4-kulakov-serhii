2572108736bbe912eb85a7c540ca92b085b71da0
kulakov12345 <sergey.kulak06@gmail.com>
2026-06-03 17:29:32 +0300

    Pract1 Uploaded

---
+++ Pract1/pzpi-23-4-kulakov-serhii-pract1/Program.cs
@@ -0,0 +1,72 @@
+﻿public abstract class State
+{
+    public abstract void HandleInput(Context editor, string text);
+    public abstract void ToggleMode(Context editor);
+}
+
+public class Context
+{
+    private State _currentState;
+    public string TextBuffer { get; set; } = "";
+
+    public Context(State initialState)
+    {
+        TransitionTo(initialState);
+    }
+
+    public void TransitionTo(State state)
+    {
+        _currentState = state;
+        Console.WriteLine($"[Система] Перехід до режиму: {state.GetType().Name}");
+    }
+
+    public void Type(string text)
+    {
+        _currentState.HandleInput(this, text);
+    }
+
+    public void PressToggle()
+    {
+        _currentState.ToggleMode(this);
+    }
+}
+
+public class NormalState : State
+{
+    public override void HandleInput(Context editor, string text)
+    {
+        Console.WriteLine($"[Normal] Натиснуто '{text}'. У звичайному режимі введення тексту заблоковано.");
+    }
+
+    public override void ToggleMode(Context editor)
+    {
+        editor.TransitionTo(new InsertState());
+    }
+}
+
+public class InsertState : State
+{
+    public override void HandleInput(Context editor, string text)
+    {
+        editor.TextBuffer += text;
+        Console.WriteLine($"[Insert] Додано текст. Поточний буфер: \"{editor.TextBuffer}\"");
+    }
+
+    public override void ToggleMode(Context editor)
+    {
+        editor.TransitionTo(new PreviewState());
+    }
+}
+
+public class PreviewState : State
+{
+    public override void HandleInput(Context editor, string text)
+    {
+        Console.WriteLine("[Preview] Попередження: Режим перегляду! Редагування неможливе.");
+    }
+
+    public override void ToggleMode(Context editor)
+    {
+        editor.TransitionTo(new NormalState());
+    }
+}
